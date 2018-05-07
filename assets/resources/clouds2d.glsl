8188fd950f8baaf4b8ee63db06a34cff95995286
王建正 <809746561@qq.com>
2018-05-07 21:55:21 +0800

    cloud

--- assets/resources/clouds2d.glsl
+++ assets/resources/clouds2d.glsl
@@ -1,3 +1,4 @@
+
 #ifdef GL_ES
 precision lowp float;
 #endif
@@ -23,32 +24,95 @@ vec2 hash(vec2 p){
     return -1.0+2.0*fract(sin(p)*43758.5453123);
 }
 
-float noise(in vec2 p){
-    const float K1 = 0.366025404;
-    const float K2 = 0.211234865;
-    vec2 i = floor(p+(p.x+p.y)*K1);
-    vec2 a = p-i+(i.x+i.y)*K2;
-    vec2 o = (a.x>a.y)?vec2(1.1,0.0):vec2(0.0,1.0);
-    vec2 b = a-o+K2;
-    vec2 c = a-1.0+2.0*K2;
-    vec3 h = max(0.5-vec3(dot(a,a),dot(b,b),dot(c,c),0.0));
-    vec3 n = h*h*h*h*vec3(dot(a,hash(i+0.0)),dot(b,hash(i+o)),dot(c,hash(i+1.0)));
-    return dot(n,vec3(70.0));
+float noise( in vec2 p ) {
+    const float K1 = 0.366025404; // (sqrt(3)-1)/2;
+    const float K2 = 0.211324865; // (3-sqrt(3))/6;
+	vec2 i = floor(p + (p.x+p.y)*K1);	
+    vec2 a = p - i + (i.x+i.y)*K2;
+    vec2 o = (a.x>a.y) ? vec2(1.0,0.0) : vec2(0.0,1.0); //vec2 of = 0.5 + 0.5*vec2(sign(a.x-a.y), sign(a.y-a.x));
+    vec2 b = a - o + K2;
+	vec2 c = a - 1.0 + 2.0*K2;
+    vec3 h = max(0.5-vec3(dot(a,a), dot(b,b), dot(c,c) ), 0.0 );
+	vec3 n = h*h*h*h*vec3( dot(a,hash(i+0.0)), dot(b,hash(i+o)), dot(c,hash(i+1.0)));
+    return dot(n, vec3(70.0));	
 }
 
 float fbm(vec2 n){
-    float total = 0.0,amplitube = 0.1;
-    for(int i = 0;i<7,i++){
-        total +=noise(n)*amplitude;
+    float total = 0.0,amplitude = 0.1;
+    for(int i = 0;i<7;i++){
+        total += noise(n)*amplitude;
         n = m*n;
-        amplitube*=0.4;
+        amplitude *= 0.4;
     }
     return total;
 }
 
-void mainImage(out vec4 fragColor,in vec2 fragCoord){
-    vec2 p = fragCoord.xy/resolution.xy;
-    vec2 uv = p*vec2(resolution.x/resolution.y,1.0);
-    float stime = time*speed;
-    float q = fbm(uv*cloudscale*0.5);
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+    vec2 p = fragCoord.xy / resolution.xy;
+	vec2 uv = p*vec2(resolution.x/resolution.y,1.0);
+    float stime = time * speed;
+    float q = fbm(uv * cloudscale * 0.5);
+
+    //ridged noise shape
+	float r = 0.0;
+	uv *= cloudscale;
+    uv -= q - stime;
+    float weight = 0.8;
+    for (int i=0; i<8; i++){
+		r += abs(weight*noise( uv ));
+        uv = m*uv + stime;
+		weight *= 0.7;
+    }
+
+     float f = 0.0;
+     uv = p*vec2(resolution.x/resolution.y,1.0);
+     uv *= cloudscale;
+     uv -= q - stime;
+     weight = 0.7;
+     for(int i = 0; i<8; i++){
+         f += weight*noise( uv );
+         uv = m*uv + stime;
+         weight *= 0.6;
+     }
+
+     f *= r+f;
+
+     float c = 0.0;
+     stime = stime*speed*2.0;
+     uv = p*vec2(resolution.x/resolution.y,1.0);
+     uv *= cloudscale*2.0;
+     uv -= q-stime;
+     weight = 0.4;
+     for(int i = 0;i<7;i++){
+         c += weight*noise(uv);
+         uv = m*uv+stime;
+         weight *= 0.6;
+     }
+
+     float c1 = 0.0;
+     stime = stime*speed*3.0;
+     uv=p*vec2(resolution.x/resolution.y,1.0);
+     uv *= cloudscale*3.0;
+     uv-=q-stime;
+     weight = 0.4;
+     for(int i = 0;i<7;i++){
+         c1+=abs(weight*noise(uv));
+         uv=m*uv+stime;
+         weight *= 0.6;
+     }
+     c+=c1;
+
+     vec3 skycolour = mix(skycolour2,skycolour1,p.y);
+     vec3 cloudcolour = vec3(1.1,1.1,0.9)*clamp((clouddark+cloudlight*c),0.0,1.0);
+     f = cloudcover+cloudalpha*f*r;
+
+     vec3 result = mix(skycolour,clamp(skytint*skycolour+cloudcolour,0.0,1.0),clamp(f+c,0.0,1.0));
+    
+     fragColor = vec4(result,1.0);
+ }
+
+void main(){
+    mainImage(gl_FragColor,gl_FragCoord.xy);
 }
+
